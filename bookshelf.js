af8d4b997c1efd6995fb0c6969ea3a04cdd3b7e3
kresdl <peter.hallstrom@gmail.com>
2020-06-01 20:38:19 +0200

    bugfix

--- bookshelf.js
+++ bookshelf.js
@@ -1,13 +1,15 @@
-const { env } = process;
+'use strict'
 
-knex = require('knex')({
-  client: 'mysql',
-    connection: {
-      database: env.DATABASE,
-      host: env.DB_HOST,
-      user: env.DB_USER,
-      password: env.DB_PASSWORD
-    }
-});
+const { env } = process,
+
+  knex = require('knex')({
+    client: 'mysql',
+      connection: {
+        database: env.DATABASE,
+        host: env.DB_HOST,
+        user: env.DB_USER,
+        password: env.DB_PASSWORD
+      }
+  });
 
 module.exports = require('bookshelf')(knex);
